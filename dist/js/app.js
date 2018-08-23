cdfd4f4a8940b36d1b57bb33522c19678a62540a
Josue Arce <josuerc026@gmail.com>
2018-08-23 00:23:06 -0400

    generating image accuracy levels and edit options

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,14 +1,15 @@
 const userInput = document.querySelector('[name="user-image"]');
+const imgBoard = document.querySelector('.img-board');
 
 const fetchData = async (imgStr) => {
- var myRegexp = /,(.*)$/g;
+ let myRegexp = /,(.*)$/g;
  var matchedImgStr = myRegexp.exec(imgStr);
  let request = {
   requests:[
    {
-     image: {
-      content: matchedImgStr[1]
-     },
+    image: {
+     content: matchedImgStr[1]
+    },
     features: [
      {
       type: "LABEL_DETECTION"
@@ -23,28 +24,66 @@ const fetchData = async (imgStr) => {
    'Accept': 'application/json',
    'Content-Type': 'application/json'
   },
-  method: "POST", 
+  method: "POST",
   body: JSON.stringify(request)
  }).then(response => response.json());
 
- console.log(response);
  return response;
-}
+};
+
+const reAnalyze = async (elem) => {
+ let imgStr = elem.querySelector('img').src;
+ elem.querySelector('p').innerText = 're-analyzing...';
+ let responseArr = await fetchData(imgStr);
+ let newLabel = `${responseArr.responses[0].labelAnnotations[1].description}, accuracy: ${Math.floor(responseArr.responses[0].labelAnnotations[1].score * 100)}%`;
+ elem.querySelector('p').innerText = newLabel;
+};
+
+const deleteItem = (elem) => {
+ elem.remove();
+};
+
+const generateImages = (src, response) => {
+  let prevBoard = document.querySelector('.img-board');
+  let template =
+    `<div class="img-wrap" style="background: url(${src}) no-repeat center center / cover;">
+        <img src="${src}" data-label="${response.description}" width="200" style="display: none;">
+        <div class="object-info">
+            <p>${response.description}, accuracy: ${Math.floor(response.score * 100)}%</p>
+            <div class="btn-container">
+              <button class="re-analyze">Re-analyze</button>
+              <button class="delete-item">Delete</button>
+            </div>
+        </div>
+    </div>`;
+  prevBoard.insertAdjacentHTML('afterbegin', template);
+};
 
 const fr = (elem) => {
- let input = elem.files[0];
- return new Promise(resolve => {
-  let reader = new FileReader();
-  reader.onloadend = () => resolve(reader.result);
-  reader.readAsDataURL(input);
- })
-}
+  let file = elem.files[0];
+  return new Promise((resolve => {
+   let reader = new FileReader();
+   reader.onloadend = () => resolve(reader.result);
+   reader.readAsDataURL(file);
+  }));
+};
 
 const getBaseString = async (elem) => {
  let imgStr = await fr(elem);
- let response = await fetchData(imgStr);
- alert(response.responses[0].labelAnnotations[0].description);
-}
+ let responseArr = await fetchData(imgStr);
+ generateImages(imgStr, responseArr.responses[0].labelAnnotations[0]);
+ console.log(responseArr);
+};
+
+
+imgBoard.addEventListener('click', function (e) {
+  if(e.target.matches('.re-analyze')){
+   reAnalyze(e.target.closest('.img-wrap'));
+  }
+  if(e.target.matches('.delete-item')){
+   deleteItem(e.target.closest('.img-wrap'));
+  }
+});
 
 userInput.addEventListener('change', function(){
  getBaseString(this);
